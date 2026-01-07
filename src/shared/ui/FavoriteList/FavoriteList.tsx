e4af9b57fe0fed84805edf0cc4bd0094fd04b7c6
jun <znznun@gmail.com>
2026-01-08 01:12:28 +0900

    style: 즐겨찾기 설명 반응형

--- src/shared/ui/FavoriteList/FavoriteList.tsx
+++ src/shared/ui/FavoriteList/FavoriteList.tsx
@@ -155,11 +155,13 @@ const FavoriteList = () => {
 
   return (
     <div className="mt-6">
-      <div className="flex items-center gap-2 mb-4">
-        <StarIcon filled className="h-5 w-5 text-yellow-500" />
-        <h2 className="text-lg font-medium text-gray-500 text-nowrap">
-          즐겨찾기
-        </h2>
+      <div className="flex flex-col sm:flex-row sm:items-center gap-2 mb-4">
+        <div className="flex items-center gap-2">
+          <StarIcon filled className="h-5 w-5 text-yellow-500" />
+          <h2 className="text-lg font-medium text-gray-500 text-nowrap">
+            즐겨찾기
+          </h2>
+        </div>
         <span className="text-xs sm:text-sm text-gray-500">
           즐겨찾기는 최대 6개까지 등록하실 수 있으며, 지역명을 클릭하여 별칭을
           등록/수정할 수 있습니다.
