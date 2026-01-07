5208f98e9ce4709e0f0f723cc2126d40df6dc966
jun <znznun@gmail.com>
2026-01-07 21:56:58 +0900

    feat(search): 검색 버튼 아이콘으로 변경

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -3,5 +3,6 @@ export { default as WeatherCard } from "./WeatherCard/WeatherCard";
 export { default as WeatherCardSkeleton } from "./WeatherCardSkeleton/WeatherCardSkeleton";
 export { default as LocationIcon } from "./Icons/LocationIcon";
 export { default as BackIcon } from "./Icons/BackIcon";
+export { default as SearchIcon } from "./Icons/SearchIcon";
 export { default as WeatherSearch } from "./WeatherSearch/WeatherSearch";
 export { default as WeatherSearchResult } from "./WeatherSearchResult/WeatherSearchResult";
